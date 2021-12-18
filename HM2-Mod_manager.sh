ce1c55550e782bfb10061e0f052610060ec6cd35
SitesHub <76928074+SitesHub@users.noreply.github.com>
2021-12-18 03:05:12 +0100

    2nd Update for the mac's version
    Added:
    -User feedback;
    -Extraction support;
    -Download support;
    
    Improved performance in:
    -Uninstall
    -Reset

--- HM2-Mod_manager.sh
+++ HM2-Mod_manager.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BOLD='\033[1m'
+UNDERLINE='\033[4m'
 NORMAL='\033[0m'
 RED='\033[1;31m'
 LIGHT_RED='\033[0;31m'
@@ -14,16 +15,22 @@ helpF() {
     echo "$BOLD""Manager of mods for HM2"    
     echo
     echo "   $BOLD-i$NORMAL           Installs the mods"
+    echo "                    -$BOLD""x$NORMAL <$UNDERLINE""path/to/compressed-file$NORMAL>"
+    echo "                       Extracts the compressed file inside $BOLD/tmp$NORMAL folder and installs its contents"
+    echo
+    echo "                    <$UNDERLINE""URL$NORMAL>"
+    echo "                       Downloads the file from an url and installs it"
+    echo "                       NOTE: if it's not from $BOLD""DropBox$NORMAL, it'll ask for user's confirmation"
     echo
     echo "   $BOLD-un$NORMAL          Uninstalls the mods"
     echo
     echo "   $BOLD-r$NORMAL           Resets everything back to normal"
     echo
     echo "   $BOLD-s$NORMAL           Sets the path of the game's folder"
-    echo "                add $BOLD-b$NORMAL after the first paramater to create the backup's path"
+    echo "                    $BOLD-b$NORMAL Creates the backup's path"
     echo
     echo "   $BOLD--settings$NORMAL   Shows all paths"
-    echo "                add $BOLD-e$NORMAL to open up the settings' folder via Explorer"
+    echo "                    $BOLD-e$NORMAL Opens up the settings' folder via Finder"
     echo
     echo "   $BOLD--help$NORMAL       Shows this help page"
 
@@ -59,9 +66,18 @@ reset() {
 
     cd "$modsDirectoryFolder"
     [[ ! -z $(ls) ]] && rm -r *
+    echo Cleaned the /mods folder
 
     cd "$pathToBackup"
-    cp hlm2_music_desktop.wad "$pathToFolder"/Contents/Resources
+    originalSize=$(du -k "hlm2_music_desktop.wad" | awk '{print $1}')
+    newSize=$(cd "$pathToFolder"/Contents/Resources; du -k "hlm2_music_desktop.wad" | awk '{print $1}')
+
+    [[ "$newSize" != "$originalSize" ]] &&
+        echo Resetting the music file; \
+        cp hlm2_music_desktop.wad "$pathToFolder"/Contents/Resources
+
+    echo
+    echo Finished resetting
 
     exit 0
 
@@ -91,6 +107,7 @@ set_path() {
 
             cd $appdata
             echo "$fixedPath" > path_to_backup.dat
+            echo Set the path to the Backup\'s folder
             exit 0
 
         else
@@ -98,6 +115,7 @@ set_path() {
             mkdir .HM2-Mod_Manager && 
             cd $appdata
             echo "$fixedPath" > path_to_backup.dat
+            echo Set the path to the Backup\'s folder
             exit 0
 
         fi
@@ -122,12 +140,14 @@ set_path() {
 
         cd $appdata
         echo "$pathWithFolderName" > path_to_game_folder.dat
+        echo Set the path to the Game\'s folder
 
     else
 
         mkdir .HM2-Mod_Manager &&
         cd $appdata
         echo "$pathWithFolderName" > path_to_game_folder.dat
+        echo Set the path to the Game\'s folder
 
     fi
 
@@ -144,7 +164,9 @@ settings() {
     if [[ "$secondArgument" == "-e" ]] ;then
 
         cd ~/
+        echo Starting Finder...
         open .HM2-Mod_Manager/
+        echo Opened Finder
         exit 0
 
     fi
@@ -159,17 +181,33 @@ settings() {
 
 }
 
-install() {
+installation() {
 
-    checks
-    [[ $? == 1 ]] && exit 1
+    cd $1
+    previousFolder=
+    folders=()
+
+    # Checks if there are folders in the same folder 
+    # that have patchwad files inside them
+    for file in */*.patchwad ; do
+
+    	currentFolder="${file%/*}/ "
+
+    	[[ $currentFolder != $previousFolder ]] && folders+=($currentFolder)
+    	previousFolder=$currentFolder
+
+    done
+
 
     # Gets the amount of files needed to be installed 
     # present in the same folder as the sh file and
     # the path to the game's folder
-    patchwad=$(ls | grep -c ".patchwad")
-    wad=$(ls | grep -c ".wad")
-    pathToFolder=$(fixPath "$(cat $appdata/path_to_game_folder.dat)" && echo $fixedPath)
+    patchwad_CurrentFolder=$(ls | grep -c ".patchwad")
+    wad_CurrentFolder=$(ls | grep -c ".wad")
+    pathToFolder=$(
+        fixPath "$(cat $appdata/path_to_game_folder.dat)" && 
+        echo $fixedPath
+    )
 
 
     # Install the files inside the correct folders
@@ -177,10 +215,186 @@ install() {
     fixPath "~/Library/Application Support/HotlineMiami2/mods"
     modsDirectoryFolder=$fixedPath
 
-    [[ $patchwad != 0 ]] && mv *.patchwad "$modsDirectoryFolder"/
-    [[ $wad != 0 ]] && mv *.wad "$pathToFolder"/Contents/Resources
+    [[ $patchwad_CurrentFolder != 0 ]] && mv *.patchwad "$modsDirectoryFolder"/
+    [[ ${#folders[*]} != 0 ]] && {
+
+        # Loops through the folders found to have patchwad files inside them
+        for i in ${!folders[*]} ;do
+
+            folder=${folders[$i]}
+            mv $folder "$modsDirectoryFolder"/
+
+        done    
+
+    }
+    echo Installed patchwad file/s inside /mods
+    
+
+    [[ $wad_CurrentFolder != 0 ]] && 
+        mv *.wad "$pathToFolder"/Contents/Resources &&
+            echo Installed wad file/s inside the game\'s folder
 
     exit 0
+
+}
+
+install() {
+
+    checks
+    [[ $? == 1 ]] && exit 1
+
+    originalPath=$(pwd)
+    # Options for the parameter
+    if [[ "$secondArgument" != "" ]] ;then
+
+        nameOfFolderForExtraction="HM2-Mod_Manager"
+
+        # Creates the temporary folder if it doesn't exist then 
+        # it cleans the temporary folder if it has content inside it
+        cd /tmp
+        [[ ! $(ls | grep "$nameOfFolderForExtraction") ]] && mkdir "$nameOfFolderForExtraction"
+
+        for i in $nameOfFolderForExtraction/* ;do
+
+            [[ "$i" != "$nameOfFolderForExtraction/*" ]] && 
+                rm -r $nameOfFolderForExtraction/* &>/dev/null
+
+        done
+
+
+        # Extraction of a zip file from the terminal
+        cd "$originalPath"
+
+        [[ "$secondArgument" == "-x" ]] && {
+
+            [[ "$thirdArgument" =~ \.(zip|rar) && -f "$thirdArgument" ]] && {
+
+                echo Extracting...
+                tar -x -f $thirdArgument -C /tmp/$nameOfFolderForExtraction &>/dev/null
+                    (( $? > 0 )) &&
+                        echo &&
+                        echo "Couldn't extract contents of $thirdArgument" && exit 1
+                installation /tmp/$nameOfFolderForExtraction
+
+            } || {
+
+                echo Couldn\'t find the zip/rar file or the path is incorrect
+                exit 1
+
+            }
+
+        }
+
+
+        # Direct download from the terminal
+        # If the link is from dropbox, it'll be quiet
+        [[ "$secondArgument" =~ "https://www.dropbox.com" ]] && {
+
+            cd /tmp/$nameOfFolderForExtraction
+
+            echo Starting the download...
+            # Corrects the url first
+                correctURL=${secondArgument//?dl=0/?dl=1}
+                curl -OL "$correctURL"  
+                    (( $? > 0 )) &&
+                        echo &&
+                        echo "$RED""URL$NORMAL could be wrong" && exit 1
+
+            echo "Extracting if necessary..."
+            # Corrects the file name first
+                correctFileName=$(
+                    fileName=$(ls); 
+                    echo ${fileName//?dl=1/}
+                )
+                mv * $correctFileName
+
+            # Extracts the file and deletes the compressed file if it's extractable
+            tar -xf $correctFileName &>/dev/null && 
+                rm $correctFileName
+
+            installation /tmp/$nameOfFolderForExtraction
+
+        }
+
+        # If it's not, it'll ask the user to check 
+        # if the link downloads a zip, rar, patchwad, wad file directly
+        [[ "$secondArgument" =~ (http|https):\/\/ ]] && {
+
+            cd /tmp/$nameOfFolderForExtraction
+
+            check() {
+
+                echo "$YELLOW""$1"$NORMAL
+                echo Are you sure that this link is a direct download to a:
+                echo "-$RED""Patchwad$NORMAL file"
+                echo "-$RED""Wad$NORMAL file"
+                echo "-$RED""Zip$NORMAL file"
+                echo "-$RED""Rar$NORMAL file"
+                echo
+                read -p "(Yes|No): " answer
+
+                case $answer in
+                    "Yes")
+
+                        echo Starting the download...
+                        curl -OL "$1"
+                            (( $? > 0 )) &&
+                                echo &&
+                                echo "$RED""URL$NORMAL could be wrong" && exit 1
+
+                        echo "Extracting if necessary..."
+                        tar -xf $correctFileName &>/dev/null && 
+                            rm $correctFileName
+
+                        installation /tmp/$nameOfFolderForExtraction
+
+                    ;;
+                    Y|y)
+
+                        echo Starting the download...
+                        curl -OL "$1"
+                            (( $? > 0 )) &&
+                                echo &&
+                                echo "$RED""URL$NORMAL could be wrong" && exit 1
+
+                        echo "Extracting if necessary..."
+                        tar -xf $correctFileName &>/dev/null && 
+                            rm $correctFileName
+
+                        installation /tmp/$nameOfFolderForExtraction
+
+                    ;;
+                    "No")
+
+                        echo "Exiting then..."
+                        exit
+
+                    ;;
+                    N|n)
+
+                        echo "Exiting then..."
+                        exit
+
+                    ;;
+                    *)
+
+                        clear
+                        echo Choose something, please
+                        read
+                        clear
+                        check "$1"
+
+                    ;;
+                esac  
+
+            }
+            check "$secondArgument"
+
+        }
+
+    fi
+
+    installation "$originalPath"
 
 }
 
@@ -204,16 +418,30 @@ uninstall() {
     # Moves all mods to the current path and then cleans the "mods" folder
     cp -r ./ "$originalPath"
     [[ ! -z $(ls) ]] && rm -r *
+    echo Cleaned the /mods folder
 
-    # Goes to the game's folder and moves the music to the current path
+
+    # Checks if the music file changed by looking at the size
     cd "$pathToFolder"/Contents/Resources
-    cp hlm2_music_desktop.wad "$originalPath"
-    
+    newSize=$(du -k "hlm2_music_desktop.wad" | awk '{print $1}')
+    originalSize=$(cd "$pathToBackup"; du -k "hlm2_music_desktop.wad" | awk '{print $1}')
 
-    # Goes to pick up the backup of the default music 
-    # and puts it inside the game's folder 
-    cd "$pathToBackup"
-    cp hlm2_music_desktop.wad "$pathToFolder"/Contents/Resources
+    if [[ "$newSize" != "$originalSize" ]] ;then
+
+        # Goes to the game's folder and moves the music to the current path
+        cp hlm2_music_desktop.wad "$originalPath"
+        echo Moved the modded music to the current folder
+
+        # Goes to pick up the backup of the default music 
+        # and puts it inside the game's folder 
+        cd "$pathToBackup"
+        echo Copying the backup inside the game\'s folder...
+        cp hlm2_music_desktop.wad "$pathToFolder"/Contents/Resources
+
+    fi
+
+    echo
+    echo Finished uninstalling
 
     exit 0
 
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Hotline Miami 2 Mod Manager CLI
 
-This is a Mod Manager designed to install, uninstall mods for Hotline miami 2 without the hastle of going back and forth with folders
+This is a Mod Manager designed to install, uninstall mods for Hotline miami 2 without the hassle of going back and forth with folders and for some links.
 
 ## How it works
 
-Basically when you want to play a new level/campaign, you'll only need to put the .patchwad's and .wad's files in the same folder that the batch file is in and it'll do the work for you
+Basically when you want to play a new level/campaign, you'll only need to put the .patchwad's and .wad's files in the same folder that the bash file is in and it'll do the work for you.
+
+It can also be done through a direct extraction or link too.
 
 ## How to set it up properly
 
@@ -24,15 +26,22 @@ To set them, just use the following parameters:
 ```
 -i            Install all mods that are in the same folder
 
+                 -x <path/to/compressed-file>
+                    Extracts the compressed file inside /tmp folder and installs its contents
+
+                 <URL>
+                    Downloads the file from an url and installs it
+                    NOTE: if it's not from DropBox, it'll ask for user's confirmation
+
 -un           Uninstalls all mods that are in the same folder
 
 -r            Resets everything back to normal
 
 -s            Sets the path of the game's folder
-              add -b after the first paramater to create the backup's path
+                  -b Creates the backup's path
      
 -settings     Shows all paths
-              add -e to open up the settings' folder via Explorer
+                  -e Opens up the settings' folder via Finder
 
 --help        Shows the help page
 ```
